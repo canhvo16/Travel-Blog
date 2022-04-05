04c55c910976733e39625ab491cb99503ace1851
Kat Imperial <imperialkatherine@gmail.com>
2022-04-05 16:48:16 -0400

    added some frontend

---
+++ client/src/store/reducers/PostReducer.js
@@ -0,0 +1,16 @@
+const { GET_POSTS } = require('../types')
+
+const initialState = { 
+    posts: []
+}
+
+const PostReducer = (state = initialState, action => {
+    switch (action.type) {
+        case GET_POSTS:
+            return { ...state, posts: action.payload}
+        default:
+            return { ...state }
+    }
+})
+
+export default PostReducer
