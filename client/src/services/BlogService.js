ea737e658674784095b846f9bd011b29a6a2e2e1
canhvo16 <canhvo16@gmail.com>
2022-04-05 15:43:06 -0500

    setup redux

---
+++ client/src/services/BlogService.js
@@ -0,0 +1,35 @@
+import Client from './'
+
+export const GetPosts = async () => {
+  try {
+    const res = await Client.get('/posts')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetComments = async () => {
+  try {
+    const res = await Client.get('/comments/:id')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CreatePost = async () => {
+  try {
+    await Client.post('/posts', req.body)
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CreateComment = async () => {
+  try {
+    await Client.post('/comments/:id', req.body)
+  } catch (error) {
+    throw error
+  }
+}
---
+++ client/src/store/actions/BlogActions.js
@@ -0,0 +1,30 @@
+import { GetPosts, GetComments } from '../../services/BlogService'
+import { GET_POSTS, GET_COMMENTS } from '../types'
+
+export const LoadPosts = () => {
+  return async (dispatch) => {
+    try {
+      const posts = await GetPosts()
+      dispatch({
+        type: GET_POSTS,
+        payload: posts
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+export const LoadComments = () => {
+  return async (dispatch) => {
+    try {
+      const comments = await GetComments()
+      dispatch({
+        type: GET_COMMENTS,
+        payload: comments
+      })
+    } catch (error) {
+      throw error
+    }
+  }
+}
